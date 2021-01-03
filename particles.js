2d978958a11c86691376e9614b19e6a114600409
whereismymind <58355172+whereismym1nd@users.noreply.github.com>
2021-01-03 14:54:08 +0600

    Create particles.js

---
+++ particles.js
@@ -0,0 +1,19 @@
+https://vincentgarreau.com/particles.js/
+
+
+<style>
+//вставляем css
+</style>
+
+<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<div id="particles-js"></div>
+
+<script>
+$(document).ready(function(){
+//Вставляем эффект в нашу обложку
+$("#particles-js").appendTo('#rec99999999 .t396__filter'); });
+</script>
+
+<script>
+//вставляем js
+</script>
